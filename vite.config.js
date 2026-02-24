7f4fb8e46f47193722099ccbad4d01f9edce7de7
👋 Hi, I'm Suraj Vishwakarma <itxsurajofficial@gmail.com>
2026-02-24 10:27:48 +0530

    Modify Vite config for deployment and server port
    Updated Vite configuration for GitHub Pages deployment and server settings.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-project/', // Important for GitHub Pages deployment
   plugins: [react()],
-})
+  server: {
+    port: 5173,
+  },
+});
