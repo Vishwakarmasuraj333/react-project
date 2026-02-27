357d7924737a7df6673b3d0128b3ab3ce6a75ad8
👋 Hi, I'm Suraj Vishwakarma <itxsurajofficial@gmail.com>
2026-02-27 15:39:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/react-project/',   // MUST match your repo name
   plugins: [react()],
+  base: "/vishwakarmasuraj333.github.io/"
 })
